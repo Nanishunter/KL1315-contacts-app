5f77c8a0625356921c195736e3f1c8436900ca68
1701542-Saimia <anttiyleinen@hotmail.com>
2018-11-05 09:06:46 +0200

    Added 2 new components

--- Kli1315-contacts-app/src/app/app.component.ts
+++ Kli1315-contacts-app/src/app/app.component.ts
@@ -6,19 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name: string;
-  greeting: string;
-  greetings: any[];
-  title = 'Kli1315-contacts-app';
-
 constructor() {
-  this.greeting = 'Hello World';
-  this.greetings = [];
-  this.name = 'World';
-}
-onSubmitGreeting() {
-  const submittedGreeting = 'Hello, ' + this.name + '!';
-  this.greetings.push(submittedGreeting);
-}
-}
+}}
 
--- Kli1315-contacts-app/src/app/app.module.ts
+++ Kli1315-contacts-app/src/app/app.module.ts
@@ -1,20 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {FormsModule} from '@angular/forms';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import { ContactListComponent } from './contact/contact-list/contact-list.component';
+import { ContactListItemComponent } from './contact/contact-list-item/contact-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    ContactListItemComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
