05d8781602c9c65c1a98f56a2bbcbd3212cdc22c
1701542-Saimia <anttiyleinen@hotmail.com>
2018-12-19 11:06:46 +0200

    Modified appmodule

---
+++ app.module.ts
@@ -0,0 +1,89 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { ContactListComponent } from './contact/contact-list/contact-list.component';
+import { ContactListItemComponent } from './contact/contact-list-item/contact-list-item.component';
+import {ContactService} from './contact/service/contact.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarComponent } from './ui/toolbar/toolbar/toolbar.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatDialogModule} from '@angular/material/dialog';
+import { HttpClientModule } from '@angular/common/http';
+import {ContactProvider} from './contact/interfaces/contact-provider';
+import {environment} from '../environments/environment';
+import {ConfirmDialogComponent} from './contact/dialog/confirm-dialog/confirm-dialog.component' ;
+
+import {
+  MatButtonModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatCardModule,
+  MatListModule,
+  MatFormFieldModule,
+  MatInputModule
+} from '@angular/material';
+import {MatIconModule} from '@angular/material/icon';
+import {RouterModule, Routes} from '@angular/router';
+import { ContactDetailComponent } from './contact/contact-detail/contact-detail.component';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { from } from 'rxjs';
+import {ToolbarService} from './ui/toolbar/toolbar/toolbar.service';
+import {ContactLocalStorageService} from './contact/service/contact-local-storage.service';
+import { ContactHttpService } from './contact/service/contact-http.service';
+import { ContactMapComponent } from './contact/contact-map/contact-map.component';
+import { SafeUrlPipe } from './contact/Pipes/safe-url.pipe';
+import { ContactFileuploadComponent } from './contact/contact-fileupload/contact-fileupload.component';
+import { ErrorDialogComponent } from './contact/error-dialog/error-dialog.component';
+
+const appRoutes: Routes = [
+  {path: 'contacts', component: ContactListComponent},
+  {path: 'contacts/new', component: ContactDetailComponent},
+  {path: 'contacts/edit/:id', component: ContactDetailComponent},
+  {path: 'contacts/map', component: ContactMapComponent},
+  {path: '**', redirectTo: '/contacts', pathMatch: 'full'},
+  {path: '', redirectTo: '/contacts', pathMatch: 'full'},
+
+];
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactListComponent,
+    ContactListItemComponent,
+    ToolbarComponent,
+    ContactDetailComponent,
+    ConfirmDialogComponent,
+    ContactMapComponent,
+    SafeUrlPipe,
+    ContactFileuploadComponent,
+    ErrorDialogComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatListModule,
+    RouterModule.forRoot(appRoutes),
+    MatFormFieldModule,
+    MatSnackBarModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    MatDialogModule,
+    HttpClientModule,
+    MatDialogModule
+
+  ],
+  entryComponents: [ConfirmDialogComponent],
+  providers: [ContactService, ToolbarService, ContactLocalStorageService,
+  {provide: ContactProvider, useClass: environment.apiEnabled ? ContactHttpService : ContactLocalStorageService}],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
